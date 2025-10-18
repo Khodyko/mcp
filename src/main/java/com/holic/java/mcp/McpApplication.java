726f6cc4cb4c041852a07f6e9525c63f4f251512
khodyko <gaes1k@yandex.ru>
2025-10-18 14:38:56 +0300

    first-commit

---
+++ src/main/java/com/holic/java/mcp/McpApplication.java
@@ -0,0 +1,14 @@
+package com.holic.java.mcp;
+
+import org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class)
+public class McpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpApplication.class, args);
+	}
+
+}
